b8a5577315513ae2e8237d92545e80eb8af7b402
pfl <pfl@eal.dk>
2011-11-16 11:37:47 +0100

    added test for ex 5.1 broke ex 5.1 to test that the test fails

---
+++ chapter5/py_chapter5/src/test_ex5.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 16, 2011
+
+@author: ubuntu
+'''
+import unittest
+import ex_5
+
+
+class Test(unittest.TestCase):
+
+
+    def testEx51_correct_input(self):
+        self.assertEqual(1, ex_5.do_plus(0,1), "do_plus returned wrong result")
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testEx51_correct_input']
+    unittest.main()
