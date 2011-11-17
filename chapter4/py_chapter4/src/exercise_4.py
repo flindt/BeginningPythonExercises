4f96517a5d610acec2b9acf047c98413b8cbf273
PFL <pfl@eal.dk>
2011-11-17 11:12:26 +0100

    first part of ex 4.1 only one number

---
+++ chapter4/py_chapter4/src/exercise_4.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 17, 2011
+
+@author: ubuntu
+'''
+
+
+    
+def printTrueFalse( X ):
+    if X:
+        print("%i is true"%X)
+    else:
+        print("%i is false"%X)
+        
+printTrueFalse(0)
