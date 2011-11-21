511cbec4e2b5846ca1fdaf1990aedacd14c59fb4
pfl <pfl@eal.dk>
2011-11-21 11:29:43 +0100

    added a few of the examples from chapter6

---
+++ chapter6/py_chapter6/src/chapter6_examples.py
@@ -0,0 +1,36 @@
+'''
+Created on Nov 21, 2011
+
+This file shows some examples of how to use the classes presented in this chapter.
+
+@author: flindt
+'''
+
+# import the classes 
+from Ch6_classes import Omelet,Fridge
+
+
+# making an Omelet
+# Or in python speak: Instatiating an object of the class "Omelet"
+o1 = Omelet()
+print( o1 )
+print( o1.get_kind() )
+
+# one more
+o2 = Omelet()
+print( o2 )
+print( o2.get_kind() )
+
+# we need a fridge to get ingredients from
+f1 = Fridge()
+print( f1 )
+
+# try to get ingredients from fridge 
+o1.get_ingredients(f1)
+
+# try to make the omelet
+# this wont work since the fridge is empty
+# o1.mix()      
+
+      
+
