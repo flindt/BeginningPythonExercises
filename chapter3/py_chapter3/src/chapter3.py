29a9dc47bf96c060fae123f3750efccefbc179ba
pfl <pfl@eal.dk>
2011-11-22 14:11:37 +0100

    done ex 3.4 3.1 to 3.4 now done

--- chapter3/py_chapter3/src/chapter3.py
+++ chapter3/py_chapter3/src/chapter3.py
@@ -9,6 +9,11 @@ dairy_section = ["milk","cream","cheese","butter"]
 
 print( "%s %s"%(dairy_section[0],dairy_section[-1]))
 
+# ex 3.3
 milk_expiration = (11,31,2011)
+
+# ex 3.4
+print( "The milk will expire on : %i / %i / %i"%milk_expiration)
+
 
 
