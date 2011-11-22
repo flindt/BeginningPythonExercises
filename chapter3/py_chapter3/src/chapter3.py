46809b78aef1b57e9c28537be98225ec936dfd63
pfl <pfl@eal.dk>
2011-11-22 14:22:37 +0100

    ex 3.6 done

--- chapter3/py_chapter3/src/chapter3.py
+++ chapter3/py_chapter3/src/chapter3.py
@@ -15,5 +15,16 @@ milk_expiration = (11,31,2011)
 # ex 3.4
 print( "The milk will expire on : %i / %i / %i"%milk_expiration)
 
+# ex 3.5 
+milk_carton = { "expiration_date" : milk_expiration, "size":5, "cost":22.5, "brand":"Thise"}
+
+# ex 3.6
+print( " This is %i oz of %s milk. It will expire on : %i / %i / %i"%( milk_carton["size"],\
+                                                                       milk_carton["brand"],\
+                                                                       milk_carton["expiration_date"][0],\
+                                                                       milk_carton["expiration_date"][1],\
+                                                                       milk_carton["expiration_date"][2]
+                                                                       ))
+
 
 
