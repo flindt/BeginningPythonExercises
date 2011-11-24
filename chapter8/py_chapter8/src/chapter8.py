dc26be960b63bfa97a0a92bdaee2770292e1a3f6
pfl <pfl@eal.dk>
2011-11-24 13:46:16 +0100

    added more calls to functions from Ch8.py

--- chapter8/py_chapter8/src/chapter8.py
+++ chapter8/py_chapter8/src/chapter8.py
@@ -8,4 +8,8 @@ import Ch8
 
 # try out the functions in the code from the book
 Ch8.print_dir(".")
+# Ch8.print_dir_by_ext(".")  # seems broken, TODO: fix this
 Ch8.print_line_lengths()
+print(Ch8.split_fully("/home/user/workspace"))
+Ch8.print_tree("../../")
+
