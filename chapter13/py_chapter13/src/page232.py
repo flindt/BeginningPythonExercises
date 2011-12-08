eb91613175d2ad462cc49021cb0b559ae055071c
pfl <pfl@eal.dk>
2011-12-08 11:33:43 +0100

    small change

--- chapter13/py_chapter13/src/page232.py
+++ chapter13/py_chapter13/src/page232.py
@@ -4,7 +4,6 @@ Created on Dec 8, 2011
 @author: pfl
 '''
 
-
 import tkinter as tk
 
 def result():
@@ -13,9 +12,10 @@ def result():
     
 win = tk.Frame()
 win.pack()
-tk.Label( win, text = "click Add to get the sum.").pack(side=tk.TOP)
-tk.Button( win, text = "add", command = result).pack(side=tk.LEFT)
-tk.Button( win, text = "Quit", command = win.quit).pack(side=tk.RIGHT)
+label1 = tk.Label( win, text = "click Add to get the sum.").pack(side=tk.TOP)
+button1 = tk.Button( win, text = "add", command = result, cursor="plus").pack(side=tk.LEFT)
+button2 = tk.Button( win, text = "Quit", command = win.quit).pack(side=tk.RIGHT)
+
 
 
 win.mainloop()
