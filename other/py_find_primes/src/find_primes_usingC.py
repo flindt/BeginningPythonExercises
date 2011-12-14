cdf43ecac2713d64a1c8838c640067af02f93fdf
pfl <pfl@eal.dk>
2011-12-14 11:09:19 +0100

    added a description on how to compile the c code

--- other/py_find_primes/src/find_primes_usingC.py
+++ other/py_find_primes/src/find_primes_usingC.py
@@ -5,6 +5,20 @@ Created on Dec 14, 2011
 '''
 
 
+'''
+
+In order to use a shared library written in C you will have to compile it first:
+    
+    gcc -c find_primes.c
+    gcc -shared -o lib_primes.so find_primes.o
+    
+Then you can open it from within python, if you use the complete path for it.
+
+http://richizo.wordpress.com/2009/01/25/calling-c-functions-inside-python/
+'''
+
+
+
 import ctypes
 ctypes.cdll.LoadLibrary("/home/pfl/git/beginning_C/other/find_primes/Debug/lib_primes.so")
 libc = ctypes.CDLL("/home/pfl/git/beginning_C/other/find_primes/Debug/lib_primes.so")
