0a3eb0ee8f6dd0da0ed15d367510703afc7c2b01
pfl <pfl@eal.dk>
2011-12-14 09:26:58 +0100

    new find primes project added a brute force and an optimesed algorithm

---
+++ other/py_find_primes/src/find_primes.py
@@ -0,0 +1,34 @@
+'''
+Created on Dec 14, 2011
+
+@author: pfl
+'''
+
+import math
+
+def brute(nMax):
+    
+    for i in range(2,nMax):
+        prime = True
+        for j in range(2,i):
+            if i%j == 0:
+                prime = False
+                
+        if prime :
+            print("%i"%i)
+    return
+
+def optimised_1(nMax):
+    
+    print("2")
+    for i in range(3,nMax,2):
+        prime = True
+        for j in range(2,int( math.sqrt(i) )):
+            if i%j == 0:
+                prime = False
+                
+        if prime :
+            print("%i"%i)
+    return
+
+optimised_1(1000)
