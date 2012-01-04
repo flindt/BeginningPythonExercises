f93b851bf664c13d8acd14f439902fffcd13b397
pfl <pfl@eal.dk>
2012-01-04 09:16:20 +0100

    added writing a few lines of an Zero signal

--- other/py_week1_handin/src/week1_handin.py
+++ other/py_week1_handin/src/week1_handin.py
@@ -13,24 +13,40 @@ t_end = 1.0
 t_step = 0.001  
 
 
-def t():
+def tick():
     '''
     returns a new time for each tick of the signal
     '''
-    yield 0.0
-
-
-def signal():
-    pass
-
-
-
-def write_to_file():
     
-    file_handle = open("output.dat",'w')
-    file_handle.write("time / ms \t output / V")
+    # using "yield" a function can return a value without losing the state it is in
+    # next time the function is called it will resume (start again) right after the "yield"
+    yield 0.0
+    yield 0.001
+    yield 0.002
+
+
+def signal(t):
+    return 0.0
+
+
+
+def write_to_file(output_filename="output.dat", signals = ()):
+    '''
+    open the output file, generates the time and signals by calling the respective functions
+    the parameter outputfilename allows the caller to set the output filename
+    
+    '''
+    file_handle = open(output_filename,'w')
+    file_handle.write("time / ms \toutput / V\n")
+    
+    for t in tick():
+        print("%f \t%f"%(t, signal(t) ))
+        file_handle.write("%f \t%f\n"%(t, signal(t) ))
+    
     
     file_handle.close()
+    
+    
     
     
 write_to_file()
