60f4b823c68fcace76aded4b7bde5eceabb30ed3
pfl <pfl@eal.dk>
2012-01-04 09:03:20 +0100

    added a few empty functions and some constants

--- other/py_ex_simple_QT/src/week1_handin.py
+++ other/py_ex_simple_QT/src/week1_handin.py
@@ -4,12 +4,28 @@ Created on Jan 4, 2012
 @author: pfl
 '''
 
+signal_types = (ZERO, NOISE, SINUS) = (0,1,2)
+
+# Define start and end time, and the time step
+# The unit used is seconds 
+t_start = 0.0
+t_end = 1.0
+t_step = 0.001  
+
+
 def t():
     '''
     returns a new time for each tick of the signal
     '''
-    return 0.0
+    yield 0.0
 
 
+def signal():
+    pass
+
+
+
+def write_to_file():
+    pass
 
 
