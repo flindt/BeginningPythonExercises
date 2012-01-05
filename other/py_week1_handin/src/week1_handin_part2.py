e632e9e99822bc23b169285b14e53e44991e9b91
pfl <pfl@eal.dk>
2012-01-05 11:02:22 +0100

    can read from file

--- other/py_week1_handin/src/week1_handin_part2.py
+++ other/py_week1_handin/src/week1_handin_part2.py
@@ -4,3 +4,21 @@ Created on Jan 4, 2012
 @author: pfl
 '''
 
+
+
+def read_from_file( input_file_name = "output.dat"):
+    input_signal = []
+    
+    file_handle = open( input_file_name, 'r')
+    
+    for this_line in file_handle.readlines():
+        try:
+            input_signal.append( (float(this_line.split()[0]), float(this_line.split()[1])) )
+        except (ValueError,IndexError):
+            pass
+        
+    
+    file_handle.close()
+    return input_signal
+
+print (read_from_file())
