a1ed832ab3a955f4d5a1954e4765ab7b553232e0
Flindt <pfl@eal.dk>
2012-11-22 10:25:29 +0100

    alternate binary add solution - not working

--- other/py_unittesting/binary_calculus/src/binaryCalculus.py
+++ other/py_unittesting/binary_calculus/src/binaryCalculus.py
@@ -1,48 +1,10 @@
 
 
 def add(a, b):
-    if len(a) > len(b): # make sure la is the longest
-        la = a
-        lb = b
-    else:
-        la = b
-        lb = a
-  
-    for index in range( len(la) ):
-        lb = "0" +lb
+    la = int( a, 2)
+    lb = int( b, 2)
     
-    result = ""
-    
-    carry=0
-    for index in range( len(la)):
-        sum = 0
-        if la[-1]=="1":
-            sum = sum + 1
-        if lb[-1]=="1":
-            sum = sum + 1
-        if carry == 1:
-            sum = sum + 1
-            
-        if sum == 0:
-            result = "0" + result
-            carry = 0
-        elif sum == 1:
-            result = "1" + result
-            carry = 0
-        elif sum == 2:
-            result = "0" + result
-            carry = 1
-        else:
-            result = "1" + result
-            carry = 1
-        
-        la = la[:-1]
-        lb = lb[:-1]
-            
-    
-    if carry == 1:
-        result = "1" + result
-    
+    result = str( la + lb).format('b')
     return result
     
 
