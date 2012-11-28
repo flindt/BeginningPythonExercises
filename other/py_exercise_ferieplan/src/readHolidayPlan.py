f6a1b8ceff46bdb4b3d8b3391f0f3230d2326229
Flindt <pfl@eal.dk>
2012-11-28 08:23:53 +0100

    ferieplan how to read a file

--- other/py_exercise_ferieplan/src/readHolidayPlan.py
+++ other/py_exercise_ferieplan/src/readHolidayPlan.py
@@ -7,21 +7,7 @@ Created on Nov 27, 2012
 
 myFile = open("ferieplan2013 14.txt", encoding="utf-8")
 
-
-while True:
-    Description = myFile.readline()
-    print( Description )
+for thisLine in myFile.readlines():
+    print( thisLine )
     
-    if Description == '':
-        break
-    
-    DateLine = myFile.readline()
-    print(DateLine)
-    
-    DateParts = DateLine.split( "-" )
-    print( DateParts )
-    
-    StartDate = DateParts[0]
-    StartDate = StartDate.strip().strip(")(")
-    print( StartDate )
     
