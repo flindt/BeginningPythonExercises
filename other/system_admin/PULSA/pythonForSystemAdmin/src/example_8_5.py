d3d1fcc3381bfe829f2fcf69d3ffd88bcc7b6cbc
Flindt <pfl@eal.dk>
2012-11-28 08:09:43 +0100

    added another IP address to try to connect to with ssh

--- other/system_admin/PULSA/pythonForSystemAdmin/src/example_8_5.py
+++ other/system_admin/PULSA/pythonForSystemAdmin/src/example_8_5.py
@@ -3,10 +3,12 @@ import subprocess
 """
 A ssh based command dispatch system
 """
-machines = ["127.0.0.1"]
+machines = ["127.0.0.1", "10.140.81.83"]
 
 cmd = "uname"
 
 
 for machine in machines:
     subprocess.call("ssh root@%s %s" % (machine, cmd), shell=True)
+
+
