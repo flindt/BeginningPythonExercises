0b514aa4f670c638a34c175931bfb04e202bf1b1
Flindt <pfl@eal.dk>
2012-11-28 11:46:04 +0100

    reverted to working version added list output only parts work

--- other/py_exercise_ferieplan/src/readHolidayPlan.py
+++ other/py_exercise_ferieplan/src/readHolidayPlan.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on Nov 27, 2012
 
@@ -7,7 +8,37 @@ Created on Nov 27, 2012
 
 myFile = open("ferieplan2013 14.txt", encoding="utf-8")
 
-for thisLine in myFile.readlines():
-    print( thisLine )
+myFile.readline()
+myFile.readline()
+
+listOfHolidays = []
+while True:
+    Description = myFile.readline()
+    print( Description )
+    
+    if Description == '':
+        break
+    
+    DateLine = myFile.readline()
+    print(DateLine)
+    
+    DateParts = DateLine.split( "-" )
+    print( DateParts )
+    
+    StartDate = DateParts[0]
+    StartDate = StartDate.strip().strip(")(")
+    print( StartDate )
+    
+    EndDate = DateParts[1]
+    EndDate = EndDate.strip().strip(")(")
+    print( EndDate )
+    
+    thisHoliday = { "Description":Description, "StartDate":StartDate,"EndDate":EndDate}
+    print (thisHoliday)
+    listOfHolidays.append( thisHoliday )
+    
+print( listOfHolidays )
+
+    
     
     
