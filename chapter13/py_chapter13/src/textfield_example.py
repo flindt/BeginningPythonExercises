ca2bef2d1bd40e11e282fee0ba4b004e952d50d3
flindt <pfl@eal.dk>
2012-12-07 11:45:18 +0100

    added a text variable to change the label text

--- chapter13/py_chapter13/src/textfield_example.py
+++ chapter13/py_chapter13/src/textfield_example.py
@@ -10,9 +10,12 @@ import tkinter as tk
 
 def result():
     print( " this sum of 2+2 is : ", 2+2)
-    resultVar.set( 2+2 )
+    resultVar.set( str(int(resultVar.get())+10) )
+    textVar.set("does it work")
+
     
-    
+mainWindow = tk.Tk()
+mainWindow.title("testing...")
     
 # Create a fraom to hold all the GUI elements
 win = tk.Frame()    
@@ -20,11 +23,14 @@ win = tk.Frame()
 # StringVar is the connection from the GUI to the code in this file
 # Update them with .set in the callback functions  
 resultVar = tk.StringVar()
-resultVar.set("trying...")
+resultVar.set("99")
+
+textVar = tk.StringVar()
+textVar.set("starting...")
 
 
 win.pack()
-label1 = tk.Label( win, text = "click Add to get the sum.").pack(side=tk.TOP)
+label1 = tk.Label( win, textvariable=textVar).pack(side=tk.TOP)
 button1 = tk.Button( win, text = "add", command = result, cursor="plus").pack(side=tk.LEFT)
 button2 = tk.Button( win, text = "Quit", command = win.quit).pack(side=tk.RIGHT)
 textEntry1 = tk.Entry( win,  textvariable=resultVar).pack(side=tk.BOTTOM)
